f79feed2d907244b33f13bfa20a6e535a7b230ce
hamedkhodami <hamedkhodami977@gmail.com>
2025-01-10 23:55:48 +0330

    update 4

--- src/static/assets/tailwind.config.js
+++ src/static/assets/tailwind.config.js
@@ -1,50 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.{html,js}"],
+  content: [],
   theme: {
-    extend: {
-      fontFamily:{
-        "INTER" : "INTER",
-        "INTER regular" : "INTER Regular",
-        "INTER Bold" : "INTER Bold",
-      },
-
-      container:{
-        "center":true,
-         padding:{
-          DEFULT:"1rem",
-          lg:"0.625rem"
-         }
-      },
-
-      backgroundImage:{
-        "home-desktop":"url(../images/boostiloaboutback.webp)",
-        "boostilo-portfolio":"url(../images/portfolio.webp)",
-        "backcomment":"url(../images/teambackground.webp)",
-        "backservices":"url(../images/servicesbackground.webp)",
-        'custom-back-services': 'url(../images/servicesback.webp)',
-        "backcontact":"url(../images/custumback2.png)"
-      },
-    },
-    screens:{
-      'sm': '640px',
-      'md': '768px',
-      'lg': '1024px',
-      'xl': '1150px',
-    },
-    keyframes:{ 
-      fadeIn: { 
-        '0%': { opacity: 0 },
-       '100%': { opacity: 1 },
-       },
-       },
-      animation: {
-         fadeIn: 'fadeIn 1s ease-in-out forwards',
-       },
+    extend: {},
   },
-  variants: {},
-  plugins: [
-    require('tailwind-clip-path'),
-  ],
+  plugins: [],
 }
 
