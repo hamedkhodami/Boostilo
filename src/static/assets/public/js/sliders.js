bb9b8b832c4ff324320b7f84a953a0006153fb73
faezehporhasani <faezeh98700@gmail.com>
2025-01-22 05:24:08 -0800

    creat new slider comment user

--- src/static/assets/public/js/sliders.js
+++ src/static/assets/public/js/sliders.js
@@ -1,11 +1,4 @@
-    var swiper = new Swiper(".swiper", {
-        loop: true,
-      navigation: {
-        nextEl: ".swiper-button-prev-custom",
-        prevEl: ".swiper-button-next-custom",
-      },
-    });
-
+ 
 
     const swiper2 = new Swiper("#jjj", {
         direction: 'horizontal',
@@ -19,6 +12,11 @@
         nextEl: ".swiper-button-prev-custom",
         prevEl: ".swiper-button-next-custom",
       },
+      loop: true,
+      navigation: {
+        nextEl: ".swiper-button-prev-custom",
+        prevEl: ".swiper-button-next-custom",
+      }
     });
 
     const swiperblog = new Swiper(".blog", {
@@ -52,3 +50,37 @@
         },
      
   });
+  
+
+  const swipercommentuser = new Swiper("#user-comment", {
+    direction: 'horizontal',
+    autoplay: {
+      delay: 2000,
+      loop:true,
+    },
+    breakpoints: {
+      340: {
+        slidesPerView:2,
+        spaceBetween: 10, 
+        
+      },
+        600: {
+          slidesPerView:2,
+          spaceBetween: 10, // <- doesn't work
+        },
+        700: {
+          slidesPerView:2,
+          spaceBetween: 10, // <- doesn't work
+        },
+        992: {
+          slidesPerView:2,
+          spaceBetween: 10, // <- doesn't work
+        },
+        1200: {
+            slidesPerView: 2,
+            spaceBetween: 10, // <- doesn't work
+  
+          },
+      },
+   
+});
