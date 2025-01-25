23efb3effa88f9c1579c6a95b7e2ef0b6091ee8d
faezehporhasani <faezeh98700@gmail.com>
2025-01-25 12:06:37 -0800

    update

--- src/static/assets/public/js/sliders.js
+++ src/static/assets/public/js/sliders.js
@@ -13,13 +13,29 @@
           delay: 2000,
           loop:true,
         },
-        slidesPerView: 2,
-        spaceBetween: 10,
-        navigation: {
-        nextEl: ".swiper-button-prev-custom",
-        prevEl: ".swiper-button-next-custom",
-      },
-      loop: true,
+        breakpoints: {
+          340: {
+            slidesPerView:1,
+  
+          },
+            600: {
+              slidesPerView:2,
+              spaceBetween: 10, // <- doesn't work
+            },
+            700: {
+              slidesPerView:2,
+              spaceBetween: 10, // <- doesn't work
+            },
+            992: {
+              slidesPerView:2,
+              spaceBetween: 10, // <- doesn't work
+            },
+            1200: {
+                slidesPerView: 2,
+                spaceBetween: 10, // <- doesn't work
+  
+              },
+          },
       navigation: {
         nextEl: ".swiper-button-prev-custom",
         prevEl: ".swiper-button-next-custom",
