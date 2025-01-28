91663a52c3812a5daac43c8e812cbe8afd4c313c
hamedkhodami <hamedkhodami977@gmail.com>
2025-01-28 19:10:32 +0330

    update30:ckeditor

--- src/config/settings/base.py
+++ src/config/settings/base.py
@@ -166,9 +166,22 @@ CKEDITOR_CONFIGS = {
         'extraPlugins': ','.join([
             'codesnippet',
             'image2',
+            'colorbutton',
         ]),
         'filebrowserUploadUrl': '/ckeditor/upload/',
         'filebrowserBrowseUrl': '/ckeditor/browse/',
+        'toolbar': [
+            { 'name': 'document', 'items': ['Source', '-', 'Save', 'NewPage', 'Preview', 'Print', '-', 'Templates' ] },
+            { 'name': 'clipboard', 'items': [ 'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo' ] },
+            { 'name': 'editing', 'items': [ 'Find', 'Replace', '-', 'SelectAll', '-', 'Scayt' ] },
+            { 'name': 'basicstyles', 'items': [ 'Bold', 'Italic', 'Underline', 'Strike', '-', 'RemoveFormat' ] },
+            { 'name': 'paragraph', 'items': [ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'BidiLtr', 'BidiRtl' ] },
+            { 'name': 'links', 'items': [ 'Link', 'Unlink', 'Anchor' ] },
+            { 'name': 'insert', 'items': [ 'Image', 'Table', 'HorizontalRule', 'SpecialChar', 'PageBreak', 'Iframe' ] },
+            { 'name': 'styles', 'items': [ 'Styles', 'Format', 'Font', 'FontSize' ] },
+            { 'name': 'colors', 'items': [ 'TextColor', 'BGColor' ] },
+            { 'name': 'tools', 'items': [ 'Maximize', 'ShowBlocks' ] },
+        ],
     },
 }
 #----------------------------------------------------------------
