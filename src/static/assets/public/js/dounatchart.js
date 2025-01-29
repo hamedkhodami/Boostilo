dd2bef75fab2bec303368951186ae5aa6b93d527
faezehporhasani <faezeh98700@gmail.com>
2025-01-29 03:53:25 -0800

    updates for landing

--- src/static/assets/public/js/dounatchart.js
+++ src/static/assets/public/js/dounatchart.js
@@ -1,18 +1,108 @@
+    // function drawCenterText(chart) {
+    //     var width = chart.width,
+    //         height = chart.height,
+    //         ctx = chart.ctx;
+    //     ctx.restore();
+    //     var centerX = width / 2;
+    //     var centerY = height / 2;
+    //     var innerRadius = (chart.chartArea.left + chart.chartArea.right) / 2 * 0.35;
+    
+    //     ctx.beginPath();
+    //     ctx.arc(centerX, centerY, innerRadius + width * 0.05, 0, 2 * Math.PI);
+    //     ctx.fillStyle = 'white';
+    //     ctx.fill();
+    //     ctx.closePath();
+    
+    //     var fontSize = window.innerWidth <= 768 ? 20 : 25;
+    //     ctx.font = fontSize + "px sans-serif";
+    //     ctx.textBaseline = "middle";
+    //     var textLines = ["Content", "Marketing", "Cycle"];
+    //     var lineHeight = fontSize * 1.2;
+    //     textLines.forEach(function(text, index) {
+    //         var textX = Math.round((width - ctx.measureText(text).width) / 2);
+    //         var textY = centerY - (lineHeight * (textLines.length - 1) / 2) + (index * lineHeight);
+    //         ctx.fillStyle = "#4e2984";
+    //         ctx.fillText(text, textX, textY);
+    //     });
+    //     ctx.save();
+    // }
+    
+    // var ctx = document.getElementById('myChart').getContext('2d');
+    // var myChart = new Chart(ctx, {
+    //     type: 'doughnut',
+    //     data: {
+    //         labels: [
+    //             'RESERCH AND PLANNING ',
+    //             'IDEATION',
+    //             'CONTENT CREATION',
+    //             'PUBLISHING',
+    //             'MARKETING AND BRANDING',
+    //         ],
+    //         datasets: [{
+    //             label: 'Marketing stages',
+    //             data: [20, 20, 20, 20, 20],
+    //             backgroundColor: [
+    //                 '#6b38b4',
+    //                 '#6b38b4',
+    //                 '#6b38b4',
+    //                 '#6b38b4',
+    //                 '#6b38b4'
+    //             ],
+    //             borderColor: [
+    //                 '#6b38b4',
+    //                 '#6b38b4',
+    //                 '#6b38b4',
+    //                 '#6b38b4',
+    //                 '#6b38b4'
+    //             ],
+    //             borderWidth: 1,
+    //             spacing: 20
+    //         }]
+    //     },
+    //     options: {
+    //         cutout: '50%',
+    //         responsive: true,
+    //         plugins: {
+    //             legend: {
+    //                 display: false 
+    //             },
+    //             tooltip: {
+    //                 enabled: true
+    //             },
+    //             datalabels: {
+    //                 color: '#fff',
+    //                 font: {
+    //                     size: window.innerWidth > 768 ? 18 : 11, // فونت را برای عرض بزرگتر از 768 پیکسل به 25 پیکسل تنظیم می‌کنیم 
+    //                     weight: '700'
+    //                 },
+                    
+    //                 align: 'center',  
+    //                 anchor: 'center',  
+    //                 formatter: function(value, context) {
+    //                     var labels = context.chart.data.labels[context.dataIndex].split(' ');
+    //                     return (context.dataIndex + 1) + '\n' + labels.join('\n');
+    //                 },
+    //                 padding: 0,
+    //                 textAlign: 'center'
+    //             }
+    //         }
+    //     },
+    //     plugins: [{
+    //         beforeDraw: drawCenterText
+    //     }, ChartDataLabels]
+    // });
+
     function drawCenterText(chart) {
-        var width = chart.width,
-            height = chart.height,
-            ctx = chart.ctx;
+        var width = chart.width, height = chart.height, ctx = chart.ctx;
         ctx.restore();
         var centerX = width / 2;
         var centerY = height / 2;
         var innerRadius = (chart.chartArea.left + chart.chartArea.right) / 2 * 0.35;
-    
         ctx.beginPath();
         ctx.arc(centerX, centerY, innerRadius + width * 0.05, 0, 2 * Math.PI);
         ctx.fillStyle = 'white';
         ctx.fill();
         ctx.closePath();
-    
         var fontSize = window.innerWidth <= 768 ? 20 : 25;
         ctx.font = fontSize + "px sans-serif";
         ctx.textBaseline = "middle";
@@ -31,29 +121,29 @@
     var myChart = new Chart(ctx, {
         type: 'doughnut',
         data: {
-            labels: [
-                'RESERCH AND PLANNING ',
-                'IDEATION',
-                'CONTENT CREATION',
-                'PUBLISHING',
-                'MARKETING AND BRANDING',
+            labels: [ 
+                'RESERCH AND PLANNING', 
+                'IDEATION', 
+                'CONTENT CREATION', 
+                'PUBLISHING', 
+                'MARKETING AND BRANDING', 
             ],
             datasets: [{
                 label: 'Marketing stages',
                 data: [20, 20, 20, 20, 20],
-                backgroundColor: [
-                    '#6b38b4',
-                    '#6b38b4',
-                    '#6b38b4',
-                    '#6b38b4',
-                    '#6b38b4'
+                backgroundColor: [ 
+                    '#6b38b4', 
+                    '#6b38b4', 
+                    '#6b38b4', 
+                    '#6b38b4', 
+                    '#6b38b4' 
                 ],
-                borderColor: [
-                    '#6b38b4',
-                    '#6b38b4',
-                    '#6b38b4',
-                    '#6b38b4',
-                    '#6b38b4'
+                borderColor: [ 
+                    '#6b38b4', 
+                    '#6b38b4', 
+                    '#6b38b4', 
+                    '#6b38b4', 
+                    '#6b38b4' 
                 ],
                 borderWidth: 1,
                 spacing: 20
@@ -62,22 +152,25 @@
         options: {
             cutout: '50%',
             responsive: true,
+            onClick: (evt, item) => {
+                if (item.length) {
+                    const index = item[0].index;
+                    const sections = ['research', 'smart-ideation', 'content-creation', 'publishing', 'branding'];
+                    const sectionId = sections[index];
+                    document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
+                }
+            },
             plugins: {
-                legend: {
-                    display: false 
-                },
-                tooltip: {
-                    enabled: true
-                },
+                legend: { display: false },
+                tooltip: { enabled: false },
                 datalabels: {
                     color: '#fff',
                     font: {
-                        size: window.innerWidth > 768 ? 18 : 11, // فونت را برای عرض بزرگتر از 768 پیکسل به 25 پیکسل تنظیم می‌کنیم 
+                        size: window.innerWidth > 768 ? 18 : 10,
                         weight: '700'
                     },
-                    
-                    align: 'center',  
-                    anchor: 'center',  
+                    align: 'center',
+                    anchor: 'center',
                     formatter: function(value, context) {
                         var labels = context.chart.data.labels[context.dataIndex].split(' ');
                         return (context.dataIndex + 1) + '\n' + labels.join('\n');
@@ -87,7 +180,6 @@
                 }
             }
         },
-        plugins: [{
-            beforeDraw: drawCenterText
-        }, ChartDataLabels]
+        plugins: [{ beforeDraw: drawCenterText }, ChartDataLabels]
     });
+    
