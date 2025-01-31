7ab4e58ae39121290762bf9a47cb5dfe2803ba18
faezehporhasani <faezeh98700@gmail.com>
2025-01-31 13:12:52 -0800

    updatessss

--- src/static/assets/public/js/sliders.js
+++ src/static/assets/public/js/sliders.js
@@ -10,7 +10,7 @@
     const swiper2 = new Swiper("#jjj", {
         direction: 'horizontal',
         autoplay: {
-          delay: 2000,
+          delay: 4000,
           loop:true,
         },
         breakpoints: {
@@ -76,3 +76,47 @@
           prevEl: ".swiper-button-next-custom",
         }
   });
+
+
+  const swiperteam = new Swiper(".team", {
+    direction: 'horizontal',
+    autoplay: {
+      delay:2000,
+      loop:true,
+    },
+    breakpoints: {
+      340: {
+        slidesPerView:3,
+        spaceBetween: 10,
+        
+      },
+        600: {
+          slidesPerView:3,
+          spaceBetween: 10, // <- doesn't work
+        },
+        700: {
+          slidesPerView:2,
+          spaceBetween: 10, // <- doesn't work
+        },
+        992: {
+          slidesPerView:2,
+          spaceBetween: 10, // <- doesn't work
+        },
+        1200: {
+            slidesPerView:4,
+            spaceBetween: 10, // <- doesn't work
+  
+          },
+      },
+  
+  
+  
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+     
+   
+  });
+
