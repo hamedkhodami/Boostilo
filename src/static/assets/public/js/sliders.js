5fced38846b2daeac40f355bba5de07e0459fdd6
faezehporhasani <faezeh98700@gmail.com>
2025-01-31 22:43:05 -0800

    kk

--- src/static/assets/public/js/sliders.js
+++ src/static/assets/public/js/sliders.js
@@ -78,45 +78,5 @@
   });
 
 
-  const swiperteam = new Swiper(".team", {
-    direction: 'horizontal',
-    autoplay: {
-      delay:2000,
-      loop:true,
-    },
-    breakpoints: {
-      340: {
-        slidesPerView:3,
-        spaceBetween: 10,
-        
-      },
-        600: {
-          slidesPerView:3,
-          spaceBetween: 10, // <- doesn't work
-        },
-        700: {
-          slidesPerView:2,
-          spaceBetween: 10, // <- doesn't work
-        },
-        992: {
-          slidesPerView:2,
-          spaceBetween: 10, // <- doesn't work
-        },
-        1200: {
-            slidesPerView:4,
-            spaceBetween: 10, // <- doesn't work
   
-          },
-      },
-  
-  
-  
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-     
-   
-  });
 
