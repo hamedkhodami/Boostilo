4c76bb17bbc1636e32785d0036b64145d87a57ee
faezehporhasani <faezeh98700@gmail.com>
2025-02-03 04:34:52 -0800

    seopage and team in aboutus and change backgroundimageteam in index and commentslider

--- src/static/assets/public/js/sliders.js
+++ src/static/assets/public/js/sliders.js
@@ -7,45 +7,64 @@
      });
 
 
-    const swiper2 = new Swiper("#jjj", {
-        direction: 'horizontal',
-        autoplay: {
-          delay:5000,
-          loop:true,
+
+     const swiper2 = new Swiper("#jjj", {
+      direction: 'horizontal',
+      autoplay: {
+        delay: 3000,
+        disableOnInteraction: false, // برای اینکه چرخش بعد از تعامل کاربر همچنان ادامه پیدا کند
+        loop: true,
+      },
+      breakpoints: {
+        340: {
+          slidesPerView: 1,
         },
-        breakpoints: {
-          340: {
-            slidesPerView:1,
-  
-          },
-            600: {
-              slidesPerView:2,
-              spaceBetween: 10, // <- doesn't work
-            },
-            700: {
-              slidesPerView:2,
-              spaceBetween: 10, // <- doesn't work
-            },
-            992: {
-              slidesPerView:2,
-              spaceBetween: 10, // <- doesn't work
-            },
-            1200: {
-                slidesPerView: 2,
-                spaceBetween: 10, // <- doesn't work
-  
-              },
-          },
+        600: {
+          slidesPerView: 2,
+          spaceBetween: 10,
+        },
+        700: {
+          slidesPerView: 2,
+          spaceBetween: 10,
+        },
+        992: {
+          slidesPerView: 2,
+          spaceBetween: 10,
+        },
+        1200: {
+          slidesPerView: 2,
+          spaceBetween: 10,
+        },
+      },
       navigation: {
         nextEl: ".swiper-button-prev-custom",
         prevEl: ".swiper-button-next-custom",
-      }
-    });
+      },
+      on: {
+        init: function () {
+          this.slides.forEach((slide) => {
+            slide.addEventListener('mouseenter', () => {
+              this.autoplay.stop();
+            });
+            slide.addEventListener('mouseleave', () => {
+              this.autoplay.start();
+            });
+            slide.addEventListener('touchstart', () => { // برای توقف چرخش در دستگاه های لمسی
+              this.autoplay.stop();
+            });
+            slide.addEventListener('touchend', () => { // برای ادامه چرخش در دستگاه های لمسی
+              this.autoplay.start();
+            });
+          });
+        },
+      },
+     });
 
+     
     const swiperblog = new Swiper(".blog", {
       direction: 'horizontal',
       autoplay: {
-        delay: 2000,
+        delay: 4000,
         loop:true,
       },
       breakpoints: {
